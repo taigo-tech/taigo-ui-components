99b273d3b46c4987b9558098272d1e349f9f608d
Melvin Tan <melvin.tan@laputa.com.my>
2020-03-06 15:15:45 +0800

    Include sass including example

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 
-import { Drawer } from 'taigo-ui-components'
+import { Drawer, ProfileHeader } from 'taigo-ui-components'
 
 export default class App extends Component {
   render () {
     return (
       <div>
         <Drawer text='Drawer' />
+        <ProfileHeader text='ProfileHeader' />
+
       </div>
     )
   }
