9879872551214ac6f0d1f2178ff535829fd93b32
Cas Chan <cas.chan@juicetech.io>
2020-04-10 21:58:09 +0800

    Improved formik example

--- example/src/demo-views/FormikExample.js
+++ example/src/demo-views/FormikExample.js
@@ -2,7 +2,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import { Field, Formik, Form } from 'formik';
 import { fieldToTextField } from 'formik-material-ui';
 import React from 'react';
-import { TextInput, EditableCard, RoundedButton, Switch } from 'taigo-ui-components';
+import { TextInput, EditableCard, Switch } from 'taigo-ui-components';
 export default () => {
     const textInput = props => <TextInput {...fieldToTextField(props)} />;
 
@@ -11,26 +11,33 @@ export default () => {
             <Formik
                 enableReinitialize={true}
                 initialValues={{ select: 0, text: 'Text', switch: false }}
-                onSubmit={(values) => { console.log(values) }}
+                onSubmit={(values, { setSubmitting }) => {
+                    console.log(values);
+                    setTimeout(() => {
+                        setSubmitting(false);
+                    }, 1000);
+                }}
             >
-                {({ resetForm, isSubmitting, submitForm }) => (
-                    <EditableCard>
+                {({ resetForm, isSubmitting, submitForm, values }) => {console.log(values); return (
+                    <EditableCard
+                        title="Formik"
+                        isLoading={isSubmitting}
+                        onAccept={done => { submitForm(); done(); }}
+                        onCancel={done => { resetForm(); done(); }}
+                    >
                         <Form>
-                            <Field name="select" label="Select" as="select" select
-                                component={textInput}>
+                            <Field name="select" label="Select" as="select" select component={textInput}>
                                 <MenuItem value={0}>A</MenuItem>
                                 <MenuItem value={1}>B</MenuItem>
                                 <MenuItem value={2}>C</MenuItem>
                             </Field>
-
+                            <br />
                             <Field name="text" label="Text" component={textInput} />
-
+                            <br />
                             <Field name="switch" component={({ field, form, ...props }) => <Switch {...field} {...props} />} />
-
-                            <RoundedButton onClick={() => { submitForm() }}>Submit</RoundedButton>
                         </Form>
                     </EditableCard>
-                )}
+                )}}
             </Formik>
         </div>
     )
