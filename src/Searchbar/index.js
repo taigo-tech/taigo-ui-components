5de04d0b9964f084d03e6ed7f7a2e013f5b70ebb
Cas Chan <cas.chan@juicetech.io>
2020-04-13 12:33:22 +0800

    Init Searchbar component

---
+++ src/Searchbar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme) => ({
+    
+}));
+
+const Component = () => {
+    const styles = useStyles();
+
+    return (
+        <Box></Box>
+    );
+};
+
+Component.propTypes = {
+    
+};
+
+export default Component;
