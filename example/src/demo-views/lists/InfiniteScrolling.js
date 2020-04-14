9ebe24df59e38778411dc70763501eeaf3e88186
Melvin Tan <melvin.tan@laputa.com.my>
2020-04-14 15:42:33 +0800

    Complete InfiniteList

--- example/src/demo-views/lists/InfiniteScrolling.js
+++ example/src/demo-views/lists/InfiniteScrolling.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InfiniteList } from 'taigo-ui-components';
 
+const generateData = () => {
+    const data = [];
+
+    for (var i = 0; i < 300; i++) {
+        data.push({ message: `item${i}` });
+    }
+
+    return data;
+}
+
+const data = generateData();
+
+const getData = (start, count) => {
+    return data.slice(start, start + count);
+}
+
+const itemPerLoad = 90;
+
 export default () => {
-    return <InfiniteList />
+    const [scrollIndex, setScrollIndex] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const listData = getData(0, scrollIndex + itemPerLoad - 1);
+
+    const loadMore = () => {
+        if (listData.length < data.length) {
+            setLoading(true);
+            setTimeout(() => { setScrollIndex(scrollIndex + itemPerLoad); setLoading(false); }, 3000);
+        }
+    }
+
+    return <InfiniteList onLoadMore={loadMore} loading={loading}>
+        {
+            listData.map((data, index) => {
+                return <div key={index}>{data.message}</div>
+            })
+        }
+    </InfiniteList>
 }
--- src/List/InfiniteList.js
+++ src/List/InfiniteList.js
@@ -1,24 +1,64 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
+import List from '@material-ui/core/List';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 const useStyles = makeStyles(theme => ({
-
+    loadContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: `${theme.spacing()}px`,
+    },
+    root: {
+        position: 'relative',
+        height: 'auto',
+    }
 }));
 
-const Component = () => {
+const InfiniteList = (props) => {
+    const muiStyles = useStyles();
+    const { children, onLoadMore, loading, stopLoad, ...scrollProps } = props;
+
+    const handleUpdate = (values) => {
+        const { scrollTop, scrollHeight, clientHeight } = values;
+        const pad = 100; // 100px of the bottom
+        // t will be greater than 1 if we are about to reach the bottom
+        const t = ((scrollTop + pad) / (scrollHeight - clientHeight));
+
+        if (t > 1 && !loading && !stopLoad) {
+            onLoadMore();
+        };
+    }
+
     return (
-        <Box>Infinite List</Box>
+        <Scrollbars {...scrollProps} onUpdate={handleUpdate}>
+            <div className={muiStyles.root}>
+                {children}
+                {
+                    loading && <div className={muiStyles.loadContainer}><CircularProgress /></div>
+                }
+            </div>
+        </Scrollbars>
     );
 }
 
-Component.propTypes = {
-    
+InfiniteList.propTypes = {
+    onLoadMore: PropTypes.func.isRequired,
+    data: PropTypes.arrayOf(PropTypes.element),
+    loading: PropTypes.bool,
+    //Used to force stop infinite loading if required
+    stopLoad: PropTypes.bool,
 };
 
-Component.defaultProps = {
-    
+InfiniteList.defaultProps = {
+
 }
 
-export default Component;
+export default InfiniteList;
